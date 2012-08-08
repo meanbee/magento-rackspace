b78452f8e5b23184f442231a0f5cecb3abea6604
Martin Shaw <martin.shaw@meanbee.com>
2012-08-08 11:50:25 +0100

    Added required files for the module

---
+++ code/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+class Meanbee_Rackspacecloud_Helper_Data extends Mage_Core_Helper_Abstract {
+//    public function log($message, $level = Zend_Log::DEBUG) {
+//        /** @var $config Meanbee_Rackspacecloud_Helper_Config */
+//        $config = Mage::helper('Rackspacecloud/config');
+//
+//        $module_log_active = $config->isLogEnabled();
+//        Mage::log("[meanbee_rackspacecloud] $message", $level, $config->getLogLocation(), $module_log_active);
+//    }
+}
