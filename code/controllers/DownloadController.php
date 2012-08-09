60ef14f033b65d1042585741a45469947f603fc1
Martin Shaw <martin.shaw@meanbee.com>
2012-08-09 12:45:02 +0100

    Check to see if the module is enabled and configured correctly.

--- code/controllers/DownloadController.php
+++ code/controllers/DownloadController.php
@@ -3,34 +3,37 @@ require_once "Mage/Downloadable/controllers/DownloadController.php";
 class Meanbee_Rackspacecloud_DownloadController extends Mage_Downloadable_DownloadController {
 
     /**
-     * @TODO Check that the module is enabled and properly configured before attempting to generate a temporary URL
-     *
      * @param $url
      * @param $resourceType
      */
     protected function _processDownload($url, $resourceType) {
-        if ($resourceType == Mage_Downloadable_Helper_Download::LINK_TYPE_URL) {
+        /** @var $config Meanbee_Rackspacecloud_Helper_Config */
+        $config = Mage::helper('rackspace/config');
 
-            /** @var $connection  Mage_Rackspacecloud_Model_Connection */
-            $connection = Mage::getSingleton('rackspace/connection');
+        if ($config->isEnabled() && $config->isConfigured()) {
+            if ($resourceType == Mage_Downloadable_Helper_Download::LINK_TYPE_URL) {
 
-            /** @var $helper Meanbee_Rackspacecloud_Helper_Data */
-            $helper = Mage::helper('rackspace');
+                /** @var $connection  Mage_Rackspacecloud_Model_Connection */
+                $connection = Mage::getSingleton('rackspace/connection');
 
-            if ($helper->isRelevantUrl($url)) {
-                $protected_url = $connection->getTempUrl($url);
+                /** @var $helper Meanbee_Rackspacecloud_Helper_Data */
+                $helper = Mage::helper('rackspace');
 
-                $this->getResponse()
-                    // Temporary redirect to avoid caching
-                    ->setHttpResponseCode(307)
-                    ->setHeader("Location", $protected_url);
+                if ($helper->isRelevantUrl($url)) {
+                    $protected_url = $connection->getTempUrl($url);
 
-                $this->getResponse()->clearBody();
-                $this->getResponse()->sendHeaders();
+                    $this->getResponse()
+                        // Temporary redirect to avoid caching
+                        ->setHttpResponseCode(307)
+                        ->setHeader("Location", $protected_url);
 
-                return;
-            } else {
-                $helper->log("Not relevant URL: " . $url);
+                    $this->getResponse()->clearBody();
+                    $this->getResponse()->sendHeaders();
+
+                    return;
+                } else {
+                    $helper->log("Not relevant URL: " . $url);
+                }
             }
         }
 
